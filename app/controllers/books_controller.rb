137854ab93588cba84c078eca01105a6219be31d
hys-ch <108731664+hys-ch@users.noreply.github.com>
2022-08-20 08:43:54 +0000

    〔Finish〕Bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,15 +13,31 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book)
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book)
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
     book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    book.destroy
+    flash[:notice] = "Book was succcessfully destroyed."
+    redirect_to'/books'
   end
 
   private
